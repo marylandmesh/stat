6a24fe82be5798ea2cd4e3b6d59af7702c888ae0
Luke Evers <me@lukevers.com>
2013-02-07 16:44:19 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE>
+<html>
+    <head>
+    	<title>Maryland Meshnet Statistics</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+    	<div class="paper">
+        	<div class="nums">
+            	<div class="numbers">
+                    <div class="stat"> <!--Active Maryland Nodes-->
+                    	0
+                    </div>
+                    <div class="stat"> <!--Inactive Maryland Nodes-->
+                    	0
+                    </div>
+                    <div class="stat"> <!--Total Active Nodes-->
+                    	0
+                    </div>
+                    <div class="stat"> <!--Total Inactive Nodes-->
+                        0
+                    </div>
+                </div>
+            </div>
+            <div class="names">
+                <div class="titles">
+                    Active Maryland Nodes
+                </div>
+                <div class="titles">
+                    Inactive Maryland Nodes
+                </div>
+                <div class="titles">
+                    Total Active Nodes
+                </div>
+                <div class="titles">
+                    Total Inactive Nodes
+                </div>
+            </div>
+            <div class="paper_wrap">
+            	<div class="button_wrap">
+                    <a href="#linegraph" class="ic">a</a>
+                    <a href="#piechart" class="ic">b</a>
+                    <a href="#map" class="ic">c</a>
+                    <a href="#table" class="ic">d</a>
+                </div>
+                <div class="content">
+                	content here
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
