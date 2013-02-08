82b223f3b1abebc564ec8be8bfa5e14cf18a1ba6
Luke Evers <me@lukevers.com>
2013-02-08 07:10:10 -0500

    Added more icons to the page.

--- index.php
+++ index.php
@@ -40,8 +40,11 @@
             	<div class="button_wrap">
                     <a href="#linegraph" class="ic">a</a>
                     <a href="#piechart" class="ic">b</a>
-                    <a href="#map" class="ic">c</a>
-                    <a href="#table" class="ic">d</a>
+                    <a href="#marylandmap" class="ic">c</a>
+                    <a href="#map" class="ic">d</a>
+		    <a href="#table" class="ic">e</a>
+		    <a href="#contact" class="ic">f</a>
+		    <a href="#github" class="ic">g</a>
                 </div>
                 <div class="content">
                 	content here
