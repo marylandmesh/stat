3a64a3d134805a7ae9bf021b76a6753d92886fe3
Luke Evers <me@lukevers.com>
2013-02-08 20:23:24 -0500

    Adding the basic stuff to maryland's local page

---
+++ local/maryland/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE>
+<html>
+    <head>
+    	<title>Hyperboria Statistics</title>
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+    	<div class="paper">
+        	<div class="nums">
+            	<div class="numbers">
+                    <div class="stat"> <!--Active Nodes-->
+                    	0
+                    </div>
+                    <div class="stat"> <!--Main Nodes-->
+                    	0
+                    </div>
+                    <div class="stat"> <!--....-->
+						0
+                    </div>
+                    <div class="stat"> <!--Recently Down Nodes-->
+                        0
+                    </div>
+                </div>
+            </div>
+            <div class="names">
+                <div class="titles">
+                    Active Nodes
+                </div>
+                <div class="titles">
+                    Main Nodes
+                </div>
+                <div class="titles">
+                    ....
+                </div>
+                <div class="titles">
+                    Recently Down Nodes
+                </div>
+            </div>
+            <div class="paper_wrap">
+            	<div class="button_wrap">
+                    <a href="/" class="ic" title="Home">a</a>
+                    <a href=".." class="ic" title="Meshlocals">b</a>
+                    <a href="#contact" class="ic" title="Contact">d</a>
+                    <a href="#graph" class="ic" title="Graph">e</a>
+                    <a href="#map" class="ic" title="Map">f</a>
+                </div>
+                <div class="content">
+                	
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
