9099780b74d7333ae7066f22d9c132bb9dd8cfa5
Luke Evers <me@lukevers.com>
2013-02-12 18:55:35 -0500

    Added a dir and index page for api.
    Also added a link to the api page on the main page

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
     	<title>Hyperboria Statistics</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
     	<div class="paper">
@@ -20,7 +20,7 @@
                     <div class="stat"> <!--Meshlocals.-->
                     	<?php
 							$meshlocals = 0;
-							$locals = glob("local/*", GLOB_ONLYDIR);
+							$locals = glob("../local/*", GLOB_ONLYDIR);
 							foreach($locals as $local)
 								$meshlocals++;
 							echo $meshlocals;                                
@@ -44,27 +44,17 @@
             </div>
             <div class="paper_wrap">
             	<div class="button_wrap">
+                	<a href="/" class="ic" title="Home">a</a>
                     <a href="/local/" class="ic" title="Meshlocals">b</a>
-                    <a href="/upload/" class="ic" title="Upload">c</a>
-                    <a href="/api/" class="ic" title="API">k</a>
-                    <a href="#contact" class="ic" title="Contact">d</a>
-					<a href="#chart" class="ic" title="Chart">e</a>
-                    <a href="#map" class="ic" title="Map">f</a>
                     <a href="#help" class="ic" title="Help">j</a>
                 </div>
                 <div class="content">
-                    <div id="contact" class="box">
-                    	contact
-                    </div>
-                    <div id="chart" class="box">
-                    	chart
-                    </div>
-                    <div id="map" class="box">
-                    	map
-                    </div>
-                    <div id="help" class="box">
+                	<div id="help" class="box">
                     	help
                     </div>
+                    
+                    #comingsoon
+                    
                 </div>
             </div>
         </div>
