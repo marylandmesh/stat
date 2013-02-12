5eb5de6e55f22dd9a02e91beadc7eee6961c0138
Luke Evers <me@lukevers.com>
2013-02-12 18:28:53 -0500

    Added the first go at encrypting the password.

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,12 @@
+// -------------------------------------
+// 
+//  Encrypt uses HMAC SHA256 encryption
+//  client side, and the password is to
+//  be sent encrypted when it's sent to
+//  the server, before it is submitted.
+// 
+// -------------------------------------
 function encrypt() {
-	alert(document.forms["upload"]["id"].value);
-	alert(document.forms["upload"]["password"].value);
+	var hash = CryptoJS.HmacSHA256(document.forms["upload"]["password"].value, document.forms["upload"]["id"].value);
+	document.forms["upload"]["password"].value = hash;
 }
